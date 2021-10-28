7a1ac5ff310d096ea735ca82534cecdbab1908f3
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2021-10-28 19:43:58 -0300

    first commit

---
+++ src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+
+import { JogoDaVelhaService } from './shared';
+
+@Component({
+  selector: 'app-jogo-da-velha',
+  templateUrl: './jogo-da-velha.component.html',
+  styleUrls: ['./jogo-da-velha.component.css']
+})
+
+export class JogoDaVelhaComponent implements OnInit {
+
+  constructor(private jogoDaVelhaService: JogoDaVelhaService) { }
+
+  ngOnInit(): void {
+    this.jogoDaVelhaService.inicializar();
+  }
+
+  /**
+   * Retorna se a tela de início deve ser exibida.
+   * 
+   * @return boolean
+   */
+  get showInicio(): boolean {
+    return this.jogoDaVelhaService.showInicio;
+  }
+
+
+  /**
+   * Retorna se o tabuleiro deve ser exibida.
+   * 
+   * @return boolean
+   */
+  get showTabuleiro(): boolean {
+    return this.jogoDaVelhaService.showTabuleiro;
+  }
+
+
+  /**
+   * Retorna se a tela de fim de jogo deve ser exibida.
+   * 
+   * @return boolean
+   */
+  get showFinal(): boolean {
+    return this.jogoDaVelhaService.showFinal;
+  }
+
+
+  /**
+   * Inicializa os dados de um novo jogo.
+   * 
+   * @return void
+   */
+  iniciarJogo(): void {
+    this.jogoDaVelhaService.iniciarJogo();
+  }
+
+}
