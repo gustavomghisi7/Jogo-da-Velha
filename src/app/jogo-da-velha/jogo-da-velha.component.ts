04f425e8ff282e383660ae011da4f2887a8d583f
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2021-10-29 15:54:46 -0300

    finished

--- src/app/jogo-da-velha/jogo-da-velha.component.ts
+++ src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -7,13 +7,12 @@ import { JogoDaVelhaService } from './shared';
   templateUrl: './jogo-da-velha.component.html',
   styleUrls: ['./jogo-da-velha.component.css']
 })
-
 export class JogoDaVelhaComponent implements OnInit {
 
   constructor(private jogoDaVelhaService: JogoDaVelhaService) { }
 
-  ngOnInit(): void {
-    this.jogoDaVelhaService.inicializar();
+  ngOnInit() {
+  	this.jogoDaVelhaService.inicializar();
   }
 
   /**
@@ -22,19 +21,17 @@ export class JogoDaVelhaComponent implements OnInit {
    * @return boolean
    */
   get showInicio(): boolean {
-    return this.jogoDaVelhaService.showInicio;
+  	return this.jogoDaVelhaService.showInicio;
   }
 
-
   /**
-   * Retorna se o tabuleiro deve ser exibida.
+   * Retorna se o tabuleiro deve ser exibido.
    * 
    * @return boolean
    */
   get showTabuleiro(): boolean {
-    return this.jogoDaVelhaService.showTabuleiro;
+  	return this.jogoDaVelhaService.showTabuleiro;
   }
-
 
   /**
    * Retorna se a tela de fim de jogo deve ser exibida.
@@ -42,17 +39,82 @@ export class JogoDaVelhaComponent implements OnInit {
    * @return boolean
    */
   get showFinal(): boolean {
-    return this.jogoDaVelhaService.showFinal;
+  	return this.jogoDaVelhaService.showFinal;
   }
-
 
   /**
    * Inicializa os dados de um novo jogo.
-   * 
+   *
    * @return void
    */
   iniciarJogo(): void {
-    this.jogoDaVelhaService.iniciarJogo();
+  	this.jogoDaVelhaService.iniciarJogo();
+  }
+
+  /**
+   * Realiza uma jogada ao clicar um local no tabuleiro.
+   *
+   * @param number posX
+   * @param number posY
+   * @return void
+   */
+  jogar(posX: number, posY: number): void {
+  	this.jogoDaVelhaService.jogar(posX, posY);
+  }
+
+  /**
+   * Retorna se a peça X deve ser exibida para a 
+   * coordena informada.
+   *
+   * @param number posX
+   * @param number posY
+   * @return boolean
+   */
+  exibirX(posX: number, posY: number): boolean {
+  	return this.jogoDaVelhaService.exibirX(posX, posY);
+  }
+
+  /**
+   * Retorna se a peça O deve ser exibida para a 
+   * coordena informada.
+   *
+   * @param number posX
+   * @param number posY
+   * @return boolean
+   */
+  exibirO(posX: number, posY: number): boolean {
+  	return this.jogoDaVelhaService.exibirO(posX, posY);
+  }
+
+  /**
+   * Retorna se a marcação de vitória deve ser exibida para a 
+   * coordena informada.
+   *
+   * @param number posX
+   * @param number posY
+   * @return boolean
+   */
+  exibirVitoria(posX: number, posY: number): boolean {
+  	return this.jogoDaVelhaService.exibirVitoria(posX, posY);
+  }
+
+  /**
+   * Retorna o número do jogador a jogar.
+   * 
+   * @return number
+   */
+  get jogador(): number {
+  	return this.jogoDaVelhaService.jogador;
+  }
+
+  /**
+   * Inicia um novo jogo.
+   * 
+   * @return void
+   */
+  novoJogo(): void {
+  	this.jogoDaVelhaService.novoJogo();
   }
 
 }
+
